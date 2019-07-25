2d0d0dd523787f9cfe8b0e6044f4d216126f6469
jie <edrianypuji@hotmail.com>
2019-07-25 21:06:38 +0200

    updating url

--- src/components/signIn/signIn.js
+++ src/components/signIn/signIn.js
@@ -20,7 +20,7 @@ class SignIn extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/signin', {
+    fetch('https://obscure-cliffs-19708.herokuapp.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
